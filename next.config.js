1d8ba662071037a68920b41530999b39bdda8d64
Your Name <your.email@example.com>
2025-05-20 23:54:53 +0800

    fix: 安裝 @ant-design/cssinjs 並補齊 next.config.js transpilePackages，解決 antd/rc 相關 build 問題

--- next.config.js
+++ next.config.js
@@ -4,10 +4,13 @@ const nextConfig = {
     'antd',
     '@ant-design',
     'rc-util',
+    '@rc-component/util',
     'rc-pagination',
     'rc-picker',
     'rc-notification',
     'rc-tooltip',
+    'rc-tree',
+    'rc-table',
     // 如有用到其他 rc-xxx 也可一併加進來
   ],
 };
